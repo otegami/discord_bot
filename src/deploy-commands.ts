43484cd48b77508483e2b568378f905868533ebc
otegami <a.s.takuya1026@gmail.com>
2022-08-13 18:20:31 +0900

    Implemented slash command response

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -13,11 +13,16 @@ if (!guildId) { console.log('GUILD_ID が登録されてないよ') }
 const commands = [
   new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
   new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!')
+  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 ].map((command) => command.toJSON())
 
 const rest = new REST({ version: '10' }).setToken(token)
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error)
+
+// 削除したい command の id を指定する
+rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
+  .then(() => console.log('Successfully deleted guild command'))
   .catch(console.error)
